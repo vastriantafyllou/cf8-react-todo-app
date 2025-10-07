a2648aee768091e949e0cad7b08a3763e07231ee
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-07 10:00:45 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Layout from "./components/layout/Layout.tsx";
+import ToDo from "./components/todo/ToDo.tsx";
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <ToDo/>
+      </Layout>
+    </>
+  )
+}
+
+export default App
