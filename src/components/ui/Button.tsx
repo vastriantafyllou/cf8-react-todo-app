27932ff826a5ecd2fbb122429148c44df2b45468
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-13 11:36:43 +0300

    fix(ui): add space typo to button className

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,8 +4,8 @@ const Button = ({onClick, disabled=false, label, addClasses=""} : ButtonProps) =
 
   return (
     <>
-      <button className={`bg-cf-dark-gray opacity-80 hover:opacity-100 
-      text-white px-4 py-2 rounded cursor-pointer` + addClasses}
+      <button className={` bg-cf-dark-gray opacity-80 hover:opacity-100 
+      text-white px-4 py-2 rounded cursor-pointer ` + addClasses}
         onClick={onClick}
         disabled={disabled}
       >
